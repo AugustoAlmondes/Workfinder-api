340e8fff6c019dfe98e2ecd9ea6e624c7d49030a
AugustoAlmondes <augusto7666@gmail.com>
2025-05-29 00:04:42 -0300

    add tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from './database/prisma.service';
 import { WorkfinderModuleModule } from './workfinder-module/workfinder-module.module';
 import { WorkfinderService } from './workfinder/workfinder.service';
 import { WorkfinderControllerController } from './workfinder-controller/workfinder-controller.controller';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [WorkfinderModuleModule],
+  imports: [WorkfinderModuleModule, TasksModule],
   controllers: [WorkfinderControllerController],
   providers: [WorkfinderService,PrismaService],
 })
