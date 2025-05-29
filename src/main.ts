0389958f69dae4dc0ecaec441db35b6a55ff8453
AugustoAlmondes <augusto7666@gmail.com>
2025-05-28 23:37:15 -0300

    add port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { AppModule } from './app.module';
 };
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
   // await app.listen(process.env.PORT, '0.0.0.0'); 
 }
 bootstrap();
